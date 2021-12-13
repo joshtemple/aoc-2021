3faaf2bc8d0b7f7f894a3d01285f9837e8fceec9
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2021-12-13 14:31:28 -0500

    Add solutions for Day 11

---
+++ 11b-octopi/11b-octopi.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+DIM = 10
+
+
+def is_valid(x, y):
+    return x >= 0 and x < DIM and y >= 0 and y < DIM
+
+
+def iter_neighbors(x, y):
+    for x_n in range(x - 1, x + 2):
+        for y_n in range(y - 1, y + 2):
+            if is_valid(x_n, y_n) and (x_n, y_n) != (x, y):
+                yield x_n, y_n
+
+
+def get_lit(grid: np.array, x: int, y: int, flashes: int) -> np.array:
+    energy = grid[y][x]
+    if energy > 9:
+        flashes += 1
+        grid[y][x] = 0
+        for x_n, y_n in iter_neighbors(x, y):
+            if grid[y_n][x_n] > 0:
+                grid[y_n][x_n] += 1
+                grid, flashes = get_lit(grid, x_n, y_n, flashes)
+    elif energy == 0:
+        pass
+    return grid, flashes
+
+
+with open("input.txt") as file:
+    grid = np.genfromtxt(file, delimiter=1, dtype=int)
+
+i = 0
+while True:
+    flashes = 0
+    for x in range(DIM):
+        for y in range(DIM):
+            grid[y][x] += 1
+    for x in range(DIM):
+        for y in range(DIM):
+            grid, flashes = get_lit(grid, x, y, flashes)
+    if flashes == DIM * DIM:
+        break
+    else:
+        i += 1
+
+print(i + 1)
