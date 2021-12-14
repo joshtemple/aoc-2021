ea318efd028f09a8eea475b272feedfc53f1f982
Josh Temple <joshuat@spotify.com>
2021-12-14 14:23:08 -0500

    Add solutions for Day 14

--- 14b-polymerization/14b-polymerization.py
+++ 14b-polymerization/14b-polymerization.py
@@ -1,35 +1,38 @@
-from typing import Dict
-from collections import Counter
+from collections import defaultdict, Counter
 
 STEPS = 40
-rules: Dict[str, str] = {}
+rules: dict[str, str] = {}
 
-def split(chunk: str, counts: Dict[str, int], step: int = 0) -> Dict[str, int]:
-    print(counts)
-    if chunk in rules:
-        insert = rules[chunk]
-        counts[insert] += 1
-    else:
-        return counts
-
-    if step < (STEPS - 1):
-        counts = split(chunk[0] + insert, counts, step + 1)
-        counts = split(insert + chunk[1], counts, step + 1)
-    else:
-        return counts
-
-    return counts
-
-with open('test.txt') as file:
+with open("input.txt") as file:
     template = file.readline().strip()
     file.readline()
     for line in file.readlines():
-        pair, element = line.strip().split(' -> ')
-        rules[pair] = element
+        pair, element = line.strip().split(" -> ")
+        rules[pair] = (pair[0] + element, element + pair[1])
 
-counts = Counter(template)
-for i in range(len(template) - 1):
-    chunk = template[i : i + 2]
-    counts = split(chunk, counts)
+pairs = [template[i : i + 2] for i in range(len(template) - 1)]
+counts = defaultdict(lambda: 0)
+counts.update(Counter(pairs))
 
-print(counts)
+for i in range(STEPS):
+    offspring = defaultdict(lambda: 0)
+    for pair, count in counts.items():
+        if pair in rules:
+            for each in rules[pair]:
+                offspring[each] += count
+        else:
+            offspring[pair] += count
+    counts = offspring
+
+char_counts = defaultdict(lambda: 0)
+for pair, count in counts.items():
+    for char in pair:
+        char_counts[char] += count
+
+for char in char_counts:
+    char_counts[char] = int(char_counts[char] / 2)
+
+char_counts[template[0]] += 1
+char_counts[template[-1]] += 1
+
+print(max(char_counts.values()) - min(char_counts.values()))
